780e4d1d9d973f2b720726030d8c01fe6e281a9d
Justica <atsuronreplaced@gmail.com>
2023-01-15 00:48:46 +0900

    Add view model, command

---
+++ MineSweeper/MineSweeper/Commands/DelegateCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MineSweeper.Commands
+{
+    public class DelegateCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        private Func<object?, bool> CanExecuteDelegate { get; init; }
+        private Action<object?> ExecuteCommandDelegate { get; init; }
+
+        public DelegateCommand(Action<object?> execCmd, Func<object?, bool> canExec)
+        {
+            CanExecuteDelegate = canExec;
+            ExecuteCommandDelegate = execCmd;
+        }
+
+        public DelegateCommand(Action<object?> execCmd)
+        {
+            CanExecuteDelegate = (_) => true;
+            ExecuteCommandDelegate = execCmd;
+        }
+
+        public bool CanExecute(object? parameter) => CanExecuteDelegate(parameter);
+
+        public void Execute(object? parameter) => ExecuteCommandDelegate(parameter);
+    }
+}
---
+++ MineSweeper/MineSweeper/ViewModels/GameScreenViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Game;
+using MineSweeper.Commands;
+
+namespace MineSweeper.ViewModels
+{
+    public class GameScreenViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private Field _field;
+        public Field MineField
+        {
+            get { return _field; }
+            set
+            {
+                _field = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Field)));
+            }
+        }
+
+        private FieldParameter _fieldParameter;
+        public FieldParameter FieldParam
+        {
+            get { return _fieldParameter; }
+            set
+            {
+                _fieldParameter = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(FieldParameter)));
+            }
+        }
+
+        public DelegateCommand OpenCommand { get; init; }
+        public DelegateCommand OpenAroundCommand { get; init; }
+
+        public GameScreenViewModel()
+        {
+            _fieldParameter = FieldParameter.CreateFromPreset(FieldParameterPreset.Small);
+            _field = new Field();
+
+            
+        }
+    }
+}
