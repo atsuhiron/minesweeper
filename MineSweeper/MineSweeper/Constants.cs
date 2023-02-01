d025d34ffbe06c3fe8cfec7a7db0e5c3cf394d8e
Justica <atsuronreplaced@gmail.com>
2023-02-02 01:08:46 +0900

    Add modifier

--- MineSweeper/MineSweeper/Constants.cs
+++ MineSweeper/MineSweeper/Constants.cs
@@ -1,11 +1,11 @@
 ﻿namespace MineSweeper
 {
-    public class Values
+    public static class Values
     {
         public const int cellSize = 32;
     }
 
-    public class Strings
+    public static class Strings
     {
         public const string menu = "メニュー";
         public const string newGameS = "ニューゲーム S";
